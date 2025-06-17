95542c616f15b50c0ee3c91212be3a1126313e4d
ShabarishMenon <shabarishmenon11@gmail.com>
2025-06-17 22:36:09 +0100

    Modified App.js

--- frontend-app/App.js
+++ frontend-app/App.js
@@ -14,16 +14,40 @@ import MessagesScreen from './screens/MessagesScreen';
 import CreateGroupChatScreen from './screens/CreateGroupChatScreen';
 import ChatSettingScreen from './screens/ChatSettingScreen';
 import GroupChatMembersScreen from './screens/GroupChatMembersScreen';
+import CreateItineraryScreen from './screens/CreateItineraryScreen';
+import CreateThreadScreen from './screens/CreateThreadScreen';
+import ControlPanelScreen from './screens/ControlPanelScreen';
+import BookingsScreen from './screens/BookingsScreen';
+import { Ionicons, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function MainAppTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <Ionicons name="home" size={size} color={color} />;
+          } else if (route.name === 'Search') {
+            return <Ionicons name="search" size={size} color={color} />;
+          } else if (route.name === 'Control Panel') {
+            return <MaterialIcons name="settings" size={size} color={color} />;
+          } else if (route.name === 'Bookings') {
+            return <FontAwesome name="calendar-check-o" size={size} color={color} />;
+          } else if (route.name === 'Profile') {
+            return <Ionicons name="person" size={size} color={color} />;
+          }
+          return null;
+        },
+      })}
+    >
+      <Tab.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
       <Tab.Screen name='Search' component={SearchScreen} />
-      <Tab.Screen name='Profile' component={ProfileScreen} options={{ headerTitle: ''}} />
+      <Tab.Screen name='Control Panel' component={ControlPanelScreen} />
+      <Tab.Screen name='Bookings' component={BookingsScreen} />
+      <Tab.Screen name='Profile' component={ProfileScreen} options={{ headerTitle: '' }} />
     </Tab.Navigator>
   );
 }
@@ -63,6 +87,8 @@ export default function App() {
         <Stack.Screen name="Create New Group" component={CreateGroupChatScreen} />
         <Stack.Screen name="Chat Settings" component={ChatSettingScreen} />
         <Stack.Screen name="Members" component={GroupChatMembersScreen} />
+        <Stack.Screen name="CreateItinerary" component={CreateItineraryScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="CreateThread" component={CreateThreadScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
