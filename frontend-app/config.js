8003b84e259e520a7af3439c011b0413dceb0816
ssweilee <119922122+ssweilee@users.noreply.github.com>
2025-06-19 17:51:00 +0100

    added location and bio

--- frontend-app/config.js
+++ frontend-app/config.js
@@ -1,3 +1,3 @@
 // config.js
 // insert your ngrok address here 
-export const API_BASE_URL = "https://6727-2a00-23c6-270d-ff01-2d3d-107f-4adb-68.ngrok-free.app";
+export const API_BASE_URL = "https://3166-2001-630-e4-4220-b857-4134-5279-8d7e.ngrok-free.app";
