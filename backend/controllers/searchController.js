3bd28e591193ba93d2653a638f95bfb2743e7640
ahl-hx <lamptey.hakeem@outlook.com>
2025-06-20 03:16:34 +0100

    searchCtrl update

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,33 +1,6 @@
 const User = require('../models/User');
 const Post = require('../models/Post');
 const DEFAULT_LIMIT = 20;
-/*
-exports.search = async (req, res) => {
-  const query = req.query.q
-  console.log(`${query}`);
-
-  if (!query) {
-    return res.status(400).json({ message: 'Search query is required' });
-  }
-
-  try {
-    const userResults = await User.find({
-      $or: [
-      {firstName: {$regex: query, $options: 'i' } },
-      { lastName: {$regex: query, $options: 'i' } },
-      ]
-    }).select('firstName lastName')
-
-    const postResults = await Post.find({
-      content: { $regex: query, $options: 'i' }
-    }).populate('userId', 'firstName lastName');
-
-    res.json({ users: userResults, posts: postResults });
-  } catch (err) {
-    res.status(500).json({ message: 'Search failed', error: err.message });
-  }
-};
-*/
 
 exports.searchUsers = async (req, res) => {
   const query = req.query.q?.trim();
