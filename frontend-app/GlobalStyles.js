acc27e18bdb8d6326d3ad75916551aace5d4ce41
Brian1022 <brian871022@gmail.com>
2025-06-28 18:15:24 +0100

    fix conflict in loginform

--- frontend-app/GlobalStyles.js
+++ frontend-app/GlobalStyles.js
@@ -1,8 +1,4 @@
 import { StyleSheet } from 'react-native';
-<<<<<<< HEAD
-=======
-import LinearGradient from 'react-native-linear-gradient';
->>>>>>> dev
 
 export default StyleSheet.create({
   center: {
@@ -14,25 +10,12 @@ export default StyleSheet.create({
     alignItems: 'center',
   },
   container: {
-<<<<<<< HEAD
     padding: 20,
-=======
-    //padding: spacing.medium,
-    //backgroundColor: colors.background,
->>>>>>> dev
     flex: 1,
     justifyContent: 'center',
   },
   heading: {
-<<<<<<< HEAD
     fontSize: 24,
     marginBottom: 16,
-=======
-    //fontSize: typography.heading,
-    //color: colors.primary,
-    //marginBottom: spacing.large,
-  },
-  background: {
->>>>>>> dev
   },
 });
