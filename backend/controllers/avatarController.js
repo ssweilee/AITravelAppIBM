0ba0cc2b5a52b43d2f7b933bee8fc70b482169d9
ahl-hx <lamptey.hakeem@outlook.com>
2025-07-02 13:59:40 +0100

    avatar edit

--- backend/controllers/avatarController.js
+++ backend/controllers/avatarController.js
@@ -1,4 +1,4 @@
-/*
+
 exports.uploadAvatar = (req, res) => {
    if (!req.file) {
      return res.status(400).json({ message: 'No file uploaded' });
@@ -7,23 +7,3 @@ exports.uploadAvatar = (req, res) => {
    const profilePicture = `${req.protocol}://${req.get('host')}/uploads/avatars/${req.file.filename}`;
    return res.json({ success: true, profilePicture: profilePicture });
  };
- */
-
- // controllers/avatarController.js
-const User = require('../models/User');
-
-exports.uploadAvatar = async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: 'No file uploaded' });
-  }
-  const profilePicture = `${req.protocol}://${req.get('host')}/uploads/avatars/${req.file.filename}`;
-  try {
-    const user = await User.findById(req.user.userId);
-    user.profilePicture = profilePicture;
-    await user.save();
-    res.json({ success: true, profilePicture });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Could not save avatar' });
-  }
-};
