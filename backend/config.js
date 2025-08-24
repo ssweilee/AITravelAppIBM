3df51cee308d4582c27d40c5b41ec0dd8933915f
ahl-hx <lamptey.hakeem@outlook.com>
2025-08-24 20:59:01 +0100

    final

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 
-const BASE_DOMAIN = process.env.BASE_DOMAIN || 'https://955f7f6bb18b.ngrok-free.app'; //can be modified
+const BASE_DOMAIN = process.env.BASE_DOMAIN || 'https://3f0590be9d62.ngrok-free.app'; //can be modified
 const UPLOADS_PATH = path.join(__dirname, 'uploads');
 
 module.exports = {
