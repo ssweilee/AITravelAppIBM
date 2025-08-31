3aabe41c613678c4187ce72455ff93f6a5a5d251
Brian1022 <brian871022@gmail.com>
2025-09-01 02:19:58 +0800

    Test file change

--- frontend-app/config.js
+++ frontend-app/config.js
@@ -1,7 +1,7 @@
 // config.js
 // insert your ngrok address here 
 
-export const API_BASE_URL = 'https://7be54901d667.ngrok-free.app';
+export const API_BASE_URL = 'https://0b2035192fb5.ngrok-free.app';
 //export const API_BASE_URL = 'http://localhost:3001'; // For local development
 
 
--- test/run-test.sh
+++ test/run-test.sh
@@ -1,1 +1,6 @@
-npx artillery run --record --key a9_nmmk8cj62ptbshl3xspm1rqzefyt8lrr --name "Load Test - Login, Search, Notifi, Recommend" -o result2.json test-load.yml
+npx artillery run \
+--record \
+--key a9_nmmk8cj62ptbshl3xspm1rqzefyt8lrr \
+--name "Load Test - Login, Search, Notifi, Recommend_Mac" \
+-o "result_$(date +%Y%m%d_%H%M%S).json" \
+test-load.yml
